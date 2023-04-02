b7ad154302f4166d1fb22a53a32a18fb96fc3001
John Costa <jooohn.elvis@hotmail.com>
2023-04-02 15:15:17 -0300

    Estoque Entrada: estoque_entrada_list.html. Close #16

--- gestao_estoque/urls.py
+++ gestao_estoque/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gestao_estoque.core.urls')),
     path('produto/', include('gestao_estoque.produto.urls')),
+    path('estoque/', include('gestao_estoque.estoque.urls')),
 ]
