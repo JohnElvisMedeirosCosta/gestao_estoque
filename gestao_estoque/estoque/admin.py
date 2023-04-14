63b926f7fb60845bfae0667bd1e19a6fbdfb9872
John Costa <jooohn.elvis@hotmail.com>
2023-04-13 23:53:01 -0300

    Estoque Saida: proxy model. Close #21

--- gestao_estoque/estoque/admin.py
+++ gestao_estoque/estoque/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Estoque, EstoqueItem
+from .models import Estoque, EstoqueItem, EstoqueEntrada, EstoqueSaida
 
 
 class EstoqueItemInline(admin.TabularInline):
@@ -7,8 +7,17 @@ class EstoqueItemInline(admin.TabularInline):
     extra = 0
 
 
-@admin.register(Estoque)
-class EstoqueAdmin(admin.ModelAdmin):
+@admin.register(EstoqueEntrada)
+class EstoqueEntradaAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'nf', 'funcionario',)
+    search_fields = ('nf',)
+    list_filter = ('funcionario',)
+    date_hierarchy = 'created'
+    inlines = (EstoqueItemInline,)
+
+
+@admin.register(EstoqueSaida)
+class EstoqueSaidaAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'nf', 'funcionario',)
     search_fields = ('nf',)
     list_filter = ('funcionario',)
