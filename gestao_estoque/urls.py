53412765d5fc8f9d52121d4efe0351d3a41feea7
John Costa <jooohn.elvis@hotmail.com>
2023-04-17 23:04:26 -0300

    Fazer Logout. closes #37

--- gestao_estoque/urls.py
+++ gestao_estoque/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gestao_estoque.core.urls')),
     path('produto/', include('gestao_estoque.produto.urls')),
     path('estoque/', include('gestao_estoque.estoque.urls')),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
