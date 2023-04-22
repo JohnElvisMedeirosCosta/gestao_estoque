5a53b1f850cecb1eca4b49d1825870baed83a60b
John Costa <jooohn.elvis@hotmail.com>
2023-04-22 19:07:55 -0300

    Importando CSV InMemoryUploadedFile. closes #45

--- gestao_estoque/produto/views.py
+++ gestao_estoque/produto/views.py
@@ -1,7 +1,10 @@
+import csv
+import io
+
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import CreateView, UpdateView, ListView
-from django.http import JsonResponse
-import ipdb
+from django.http import JsonResponse, HttpResponseRedirect
 
 from .forms import ProdutoForm
 from .models import Produto
@@ -42,3 +45,40 @@ def produto_json(request, pk):
     produto = Produto.objects.filter(pk=pk)
     data = [item.to_dict_json() for item in produto]
     return JsonResponse({'data': data})
+
+def save_data(data):
+    """
+    Cria objetos Produto a partir dos dados lidos do arquivo CSV e salva esses objetos no banco de dados usando o método bulk_create do ORM do Django
+    """
+    produtos = []
+    for item in data:
+        # Extrai os valores das colunas do dicionário
+        produto = item.get('produto')
+        ncm = item.get('ncm')
+        importado = True if item.get('importado') == 'True' else False
+        preco = item.get('preco')
+        estoque = item.get('estoque')
+        estoque_minimo = item.get('estoque_minimo')
+        # Cria um objeto Produto com os valores extraídos
+        produto_obj = Produto(
+            produto=produto,
+            ncm=ncm,
+            importado=importado,
+            preco=preco,
+            estoque=estoque,
+            estoque_minimo=estoque_minimo
+        )
+        produtos.append(produto_obj)
+    # Cria todos os objetos Produto de uma vez usando o método bulk_create do ORM do Django
+    Produto.objects.bulk_create(produtos)
+
+def import_csv(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        file = myfile.read().decode('latin-1')
+        reader = csv.DictReader(io.StringIO(file))
+        data = [row for row in reader]
+        save_data(data)
+        return HttpResponseRedirect(reverse('produto:produto_list'))
+    template_name = 'produto_import.html'
+    return render(request, template_name)
