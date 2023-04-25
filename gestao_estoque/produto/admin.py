a605f0ccdfa9b1c458e34d558326ac2ccbd6e710
John Costa <jooohn.elvis@hotmail.com>
2023-04-25 01:05:22 -0300

    Admin: Criando actions no Django Admin - exportando CSV. closes #54

--- gestao_estoque/produto/admin.py
+++ gestao_estoque/produto/admin.py
@@ -1,4 +1,8 @@
+import csv
+
 from django.contrib import admin
+from django.http import HttpResponse
+
 from .models import Produto, Categoria
 
 
@@ -14,12 +18,29 @@ class ProdutoAdmin(admin.ModelAdmin):
     )
     search_fields = ('produto',)
     list_filter = ('importado',)
+    actions = ('export_as_csv',)
 
     class Media:
         js = (
             'https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js',
             '/static/js/estoque_admin.js',
         )
+
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = f'attachment; filename={meta}.csv'
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = 'Exportar para CSV'
 
 
 @admin.register(Categoria)
