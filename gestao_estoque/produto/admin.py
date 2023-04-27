062fbe903982e5530b3e0087d07fd8b9bfc17177
John Costa <jooohn.elvis@hotmail.com>
2023-04-27 00:24:27 -0300

    Criando actions no Django Admin - exportando XLSX. closes #55

--- gestao_estoque/produto/admin.py
+++ gestao_estoque/produto/admin.py
@@ -1,11 +1,13 @@
 import csv
+from datetime import datetime
 
+import xlwt
 from django.contrib import admin
 from django.http import HttpResponse
 
 from .models import Produto, Categoria
 
-
+MDATA = datetime.now().strftime('%Y-%m-%d')
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
     list_display = (
@@ -18,7 +20,7 @@ class ProdutoAdmin(admin.ModelAdmin):
     )
     search_fields = ('produto',)
     list_filter = ('importado',)
-    actions = ('export_as_csv',)
+    actions = ('export_as_csv', 'export_as_xls',)
 
     class Media:
         js = (
@@ -41,6 +43,38 @@ class ProdutoAdmin(admin.ModelAdmin):
         return response
 
     export_as_csv.short_description = 'Exportar para CSV'
+
+    def export_as_xls(self, request, queryset):
+        meta = self.model._meta
+        columns = ('Produto', 'NCM', 'Importado',
+                  'Preco', 'Estoque', 'Estoque minimo', 'Categoria')
+
+        response = HttpResponse(content_type='application/ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="{}_{}.xls"'.format(meta, MDATA)
+
+        wb = xlwt.Workbook(encoding='utf-8')
+        ws = wb.add_sheet(self.model.__name__)
+
+        row_num = 0
+
+        font_style = xlwt.XFStyle()
+        font_style.font.bold = True
+
+        for col_num in range(len(columns)):
+            ws.write(row_num, col_num, columns[col_num], font_style)
+
+        default_style = xlwt.XFStyle()
+
+        rows = queryset.values_list('produto', 'ncm', 'importado', 'preco', 'estoque', 'estoque_minimo', 'categoria__categoria')
+        for row, rowdata in enumerate(rows):
+            row_num += 1
+            for col_num, col in enumerate(rowdata):
+                ws.write(row_num, col_num, col, default_style)
+
+        wb.save(response)
+        return response
+
+    export_as_xls.short_description = 'Exportar para XLS'
 
 
 @admin.register(Categoria)
